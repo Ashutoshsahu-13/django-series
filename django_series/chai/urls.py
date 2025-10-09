a87c961df6fe3463699c511945fb415d2f173d00
Ashutoshsahu-13 <ashutoshsahu243@gmail.com>
2025-10-09 16:51:01 +0530

    models and urls

--- django_series/chai/urls.py
+++ django_series/chai/urls.py
@@ -19,8 +19,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
    
-    path('',views.all_chai,name='all_home'),
-    # path("about/",views.about, name="about"),
-    # path("contact/",views.contact,name='contact'),
+    path('',views.all_chai,name='all_chai'),
+    path('<int:chai_id>/', views.chai_detail , name='chai_detail'),
+   
     
 ]
--- django_series/chai/views.py
+++ django_series/chai/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-
+from .models import chaivarity
+from django.shortcuts import get_list_or_404
 # Create your views here.
 def all_chai(request):
-    return render(request,'chai/all_chai.html')
+    chais=chaivarity.objects.all()
+    return render(request,'chai/all_chai.html',{'chais':chais})
+
+def chai_detail(request,chai_id):
+    chai=get_list_or_404(chaivarity,pk=chai_id)
+    return render(request,'chai/chai_detail.html',{'chai':chai})
+    
